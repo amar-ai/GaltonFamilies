badae5b39fd52565d6873724f0467fd426ca7979
amar-ai <67940540+amar-ai@users.noreply.github.com>
2021-03-13 19:48:26 +1100

    #Add least square estimates calculation

--- correlations.R
+++ correlations.R
@@ -30,3 +30,36 @@ female_heights %>% ggplot(aes(mother,daughter)) +
 #Compute regression line for daughter vs mother
 m_1 <- corr*sd_daughter/sd_mother #Slope of regression line
 b_1 <- mean_daughter - m_1*mean_mother
+
+
+#Least square method assessment
+galton_heights <- GaltonFamilies %>%
+  filter(gender == "male") %>%
+  group_by(family) %>%
+  sample_n(1) %>%
+  ungroup() %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight)
+
+rss <- function(beta0, beta1, data){
+  resid <- galton_heights$son - (beta0+beta1*galton_heights$father)
+  return(sum(resid^2))
+}
+
+beta1 = seq(0, 1, len=nrow(galton_heights))
+results <- data.frame(beta1 = beta1,
+                      rss = sapply(beta1, rss, beta0 = 25))
+results %>% ggplot(aes(beta1, rss)) + geom_line() + 
+  geom_line(aes(beta1, rss), col=2)
+
+#Monte Carlo simulation
+B <- 1000
+N <- 100
+lse <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>% 
+    lm(son ~ father, data = .) %>% .$coef 
+})
+
+lse <- data.frame(beta_0 = lse[1,], beta_1 = lse[2,]) 
+lse %>% ggplot(aes(beta_0)) + geom_histogram()
+lse %>% ggplot(aes(beta_1)) + geom_histogram()
