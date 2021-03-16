6e58bac7a56794630c59c8c25a6aad08cd4ae262
amar-ai <67940540+amar-ai@users.noreply.github.com>
2021-03-16 16:50:45 +1100

    #Updating correlations code

--- correlations.R
+++ correlations.R
@@ -63,3 +63,21 @@ lse <- replicate(B, {
 lse <- data.frame(beta_0 = lse[1,], beta_1 = lse[2,]) 
 lse %>% ggplot(aes(beta_0)) + geom_histogram()
 lse %>% ggplot(aes(beta_1)) + geom_histogram()
+
+##Assessment 2.3
+library(tidyverse)
+library(HistData)
+data("GaltonFamilies")
+set.seed(1) # if you are using R 3.5 or earlier
+set.seed(1, sample.kind = "Rounding") # if you are using R 3.6 or later
+galton <- GaltonFamilies %>%
+  group_by(family, gender) %>%
+  sample_n(1) %>%
+  ungroup() %>% 
+  gather(parent, parentHeight, father:mother) %>%
+  mutate(child = ifelse(gender == "female", "daughter", "son")) %>%
+  unite(pair, c("parent", "child"))
+
+galton
+
+galton %>% group_by(pair) %>% summarise(n=n())
